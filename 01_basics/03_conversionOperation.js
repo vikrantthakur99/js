d00e953b321660ed4718607a1c6a09b26de377b8
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2023-09-21 11:50:33 +0000

    operations basics

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,27 @@ let booleanValue=Boolean(isLoggedIn)
 let someNumber=33
 
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof(someNumber));
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof(someNumber));
+// console.log(typeof stringNumber);
+
+//*************************Operations**********************
+
+let str1="Hello";
+let str2=" Vikrant"
+let str3= str1+str2;
+
+// console.log(str3);
+
+//  console.log("1"+2);
+//  console.log(2+"1");
+//  console.log("1"+2+2);
+//  console.log(2+1+"2");
+
+ /*
+ =>as per the ECMA script if we use number as a string first and then
+ use number to add then it will consider as all string.
+ =>Also its opposite for if we use number first and then number and then string then it will
+ consider first two numbers and then last string.
+ */
+
