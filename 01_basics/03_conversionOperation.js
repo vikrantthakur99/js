cc9b2da1ba632c39d13fda3d36bf62657ce5ff88
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2023-09-21 11:02:03 +0000

    latest conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Vikrant"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=> 33
+// "33abc" => NaN
+// true => 1
+//false => 0
+
+let isLoggedIn =""
+
+let booleanValue=Boolean(isLoggedIn)
+//console.log(booleanValue)
+
+//1=>true
+//0=>false
+//""=>false
+//"vikrant"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof(someNumber));
+console.log(typeof stringNumber);
