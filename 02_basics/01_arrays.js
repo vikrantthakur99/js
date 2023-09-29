ccdae2cc901dd42fc97da26fd9272ff967159ffc
Vikrant Thakur <86282855+vikrantthakur99@users.noreply.github.com>
2023-09-29 10:46:18 +0000

    array basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+const myArr=[1,2,4,5,6];
+const myNewArr=["Vikrant","Zeeshan"];
+
+const myArr2=new Array(1,2,3,5);
+// console.log(myArr[4]);
+
+// Array Methods
+
+// myArr.push(7);
+// myArr.push(8);
+// myArr.pop(); //removes the last value
+
+// myArr.unshift(0); //add the element in the array in the starting of the index
+// myArr.shift(); //remove the first element in the array
+
+// console.log(myArr.includes(10)); //includes will check if the element is present in the array or not and will return the result in the boolean result
+// console.log(myArr.indexOf(22)); //if the value is not included in the then on using the indedx methodn in the array the output will be -1
+//console.log(myArr);
+
+//const newArraySecond=myArr.join();
+
+//console.log(myArr);
+//console.log(newArraySecond); 
+//console.log(typeof newArraySecond); //on joining the array we will get the string typeof 
+
+
